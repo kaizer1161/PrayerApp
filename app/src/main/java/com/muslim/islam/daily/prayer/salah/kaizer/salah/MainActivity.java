5c05aaed051deb97cacf6d5811dc739601d38e6d
kaizer <mir.rayan1@gmail.com>
2017-01-27 17:04:35 +0600

    1st commit

---
+++ app/src/main/java/com/muslim/islam/daily/prayer/salah/kaizer/salah/MainActivity.java
@@ -0,0 +1,13 @@
+package com.muslim.islam.daily.prayer.salah.kaizer.salah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
